f4bea3348a18f83b81da1f785363e1d87ab8ebc5
Peter Le <105581900+petaa1@users.noreply.github.com>
2024-12-27 00:04:38 +0800

    add icons
    - github/LinkedIn icons

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import './App.css'
 import TypewriterText from './textWriter.tsx'
+import Github from './assets/github.svg'
+import LinkedIn from './assets/linkedin.svg'
 
 function App() {
   return (
@@ -8,25 +10,26 @@ function App() {
         <div className='max-w-7xl mx-auto px-4 sm:px-6 lg:px-8'>
           <div className='text-center'>
             <h1 className='text-4xl md:text-6xl font-bold text-gray-900 mb-6'>
-              Testing
+              Peter Le
             </h1>
-            <p className='text-lg text-gray-600 mb-8 max-w-2xl mx-auto'>
-              This is a test.
+            <p className='text-lg text-gray-600 mb-2 max-w-2xl mx-auto'>
+            Recent graduate from the University of Western Australia with a focus on innovative solutions and practical applications.
             </p>
-            <TypewriterText />
+            <h1 className='text-lg'><TypewriterText/></h1>
+            
             <div className='flex justify-center'>
               <a
                 href='https://github.com/petaa1'
                 target="_blank"
                 className='text-lg font-medium text-blue-600 hover:text-blue-500'
               >
-                GitHub
+                <img src={Github} className="logo"/> GitHub
               </a>
               <a href='https://linkedin.com/in/peterle0203' 
               target="_blank"
               className='text-lg font-medium text-blue-600 hover:text-blue-500 ml-4'
               >
-                LinkedIn
+                <img src={LinkedIn} className="logo"/> LinkedIn
               </a>
             </div>
           </div>
